9c204a235b63167325440e6ac1b0fdbebb88d672
Yubo Tian <yubo.d.tian@gmail.com>
2015-08-30 12:08:10 -0400

    back end routing done. TODO: incorporate mongo

---
+++ Start/app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var passport = require('passport');
+var session = require('express-session');
+
+var cookieParser = require('cookie-parser'); 
+var bodyParser = require('body-parser');
+
+//changed parts:
+require('./models/models.js');
+
+var index = require('./routes/index');
+var api = require('./routes/api');
+var authenticate = require('./routes/authenticate')(passport);
+
+var mongoose = require('mongoose');
+//connect to mongo db
+if(process.env.DEV_ENV){
+  mongoose.connect("mongodb://localhost:27017/newsTest");
+}
+else {
+  mongoose.connect("mongodb://dbuseryubo:dbpassword1@ds062097.mongolab.com:62097/yubotian");
+}
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(session({
+  secret: 'super duper secret'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//initialize passport
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+app.use('/', index);
+app.use('/api', api);
+app.use('/auth', authenticate);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ javascripts/csaApp.js
@@ -0,0 +1,93 @@
+var app = angular.module('csaApp', ['ngRoute', 'ngResource']).run(function($http, $rootScope) {
+  $rootScope.authenticated = false;
+  $rootScope.current_user = '';
+
+  $rootScope.signout = function(){
+    $http.get('auth/signout');
+    $rootScope.authenticated = false;
+    $rootScope.current_user = '';
+  };
+});
+
+
+app.factory('postService', function($resource){
+  return $resource('/api/posts/:id');
+});
+
+
+app.controller('mainCtrl', function(){
+});
+
+
+app.controller ('newsCtrl' , function( $scope, postService ) {
+	$scope.posts = postService.query();
+	$scope.newPost = { created_by : '' , created_location : '' , text : '' , created_time : '' } ; //TODO: picture
+
+
+	$scope.post = function() {
+    $scope.newPost.created_by = "DukeCSA";
+    //alternatively: 
+    //$scope.newPost.created_by = $rootScope.current_user;
+    $scope.newPost.created_time = Date.now(); 
+
+    postService.save($scope.newPost, function(){
+      $scope.posts = postService.query();
+      $scope.newPost = { created_by: '' , created_location : '' , text : '' , created_time : '' } ;
+    });
+    
+	};
+});
+
+
+app.controller('authCtrl', function($scope , $rootScope, $http , $location){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    $http.post('/auth/login', $scope.user).success(function(data){
+      if(data.state == 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.user.username;
+        //redirect user
+        $location.path('/');
+      }
+      else{
+        $scope.error_message = data.message;
+      }
+    });
+  };
+
+  $scope.register = function(){
+    $http.post('/auth/signup', $scope.user).success(function(data){
+      if(data.state == 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.user.username;
+        $location.path('/');
+      }
+      else{
+        $scope.error_message = data.message;
+      }
+    });
+  };
+});
+
+
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: '../index.html',
+      controller: 'mainCtrl'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: '../login.html',
+      controller: 'authCtrl'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: '../register.html',
+      controller: 'authCtrl'
+    });
+});
+
